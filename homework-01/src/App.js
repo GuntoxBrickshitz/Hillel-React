2576ac5fa877d441c5cead07421d9fcad13f2a6a
Andriy Korol <king140486@gmail.com>
2020-05-24 13:13:50 +0300

    hw-01 v1.1.1

--- homework-01/src/App.js
+++ homework-01/src/App.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div
+        title="Homework"
+        className="homework"
+        style={{ paddingLeft: '30px'}}
+      >       
         Homework 01
       </div>
     )
